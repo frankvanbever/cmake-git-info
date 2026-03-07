4aa089b5fdca7b6248336d4a2c6d7d860ea3b561
Igor Mikolic-Torreira <igormt@alumni.caltech.edu>
2026-03-07 11:14:57 -0500

    Implement full git sha1, and numeric version of short sha1

--- support/version.cmake
+++ support/version.cmake
@@ -24,10 +24,14 @@ execute_process(COMMAND git rev-parse --short HEAD
 
 if("${GIT_REV}" STREQUAL "")
   set(GIT_REV "N/A")
+  set(GIT_REV_FULL "N/A")
   set(GIT_DIFF "")
   set(GIT_TAG "N/A")
   set(GIT_BRANCH "N/A")
 else()
+  execute_process(
+    COMMAND bash -c "git rev-parse HEAD"
+    OUTPUT_VARIABLE GIT_REV_FULL)
   execute_process(
     COMMAND bash -c "git diff --quiet --exit-code || echo -dirty"
     OUTPUT_VARIABLE GIT_DIFF)
@@ -37,7 +41,7 @@ else()
     COMMAND git rev-parse --abbrev-ref HEAD OUTPUT_VARIABLE GIT_BRANCH)
 
   string(STRIP "${GIT_REV}" GIT_REV)
-  string(SUBSTRING "${GIT_REV}" 1 7 GIT_REV)
+  string(STRIP "${GIT_REV_FULL}" GIT_REV_FULL)
   string(STRIP "${GIT_DIFF}" GIT_DIFF)
   string(STRIP "${GIT_TAG}" GIT_TAG)
   string(STRIP "${GIT_BRANCH}" GIT_BRANCH)
